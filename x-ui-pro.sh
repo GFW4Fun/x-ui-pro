ac6a80fa22bcc64bd4850f4b4f32528c0bfd4e79
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-06-07 20:14:21 +0330

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ############### x-ui-pro v1.3.3 @ github.com/GFW4Fun ##############
-[[ $EUID -ne 0 ]] && echo "not root!" && exit 1
+[[ $EUID -ne 0 ]] && echo "not root!" && sudo su
 Pak=$(type apt &>/dev/null && echo "apt" || echo "yum")
 msg_ok() { echo -e "\e[1;42m $1 \e[0m";}
 msg_err() { echo -e "\e[1;41m $1 \e[0m";}
