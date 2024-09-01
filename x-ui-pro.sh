7532d6407723c09d2dd48e19cbc518f6ae912ee4
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-09-01 19:28:53 +0330

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -147,7 +147,7 @@ server {
 		proxy_redirect off;
 		proxy_set_header Host \$host;
 		proxy_set_header X-Real-IP \$client_real_ip;
-		proxy_set_header X-Forwarded-For \$http_x_forwarded_for;
+		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 		proxy_pass http://127.0.0.1:$PORT;
 		break;
 	}
@@ -157,7 +157,7 @@ server {
                 proxy_redirect off;
                 proxy_set_header Host \$host;
                 proxy_set_header X-Real-IP \$client_real_ip;
-                proxy_set_header X-Forwarded-For \$http_x_forwarded_for;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
                 proxy_pass http://127.0.0.1:\$fwdport/sub/\$fwdpath\$is_args\$args;
                 break;
         }
@@ -167,7 +167,7 @@ server {
                 proxy_redirect off;
                 proxy_set_header Host \$host;
                 proxy_set_header X-Real-IP \$client_real_ip;
-                proxy_set_header X-Forwarded-For \$http_x_forwarded_for;
+                proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
                 proxy_pass http://127.0.0.1:\$fwdport/json/\$fwdpath\$is_args\$args;
                 break;
         }
@@ -188,7 +188,7 @@ server {
 		proxy_set_header Connection "upgrade";
 		proxy_set_header Host \$host;
 		proxy_set_header X-Real-IP \$client_real_ip;
-		proxy_set_header X-Forwarded-For \$http_x_forwarded_for;
+		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
 		proxy_set_header CF-IPCountry \$http_cf_ipcountry;
 		if (\$content_type ~* "GRPC") {
 			grpc_pass grpc://127.0.0.1:\$fwdport\$is_args\$args;
