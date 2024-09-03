baa3a9ebde8d8400a847dc3438586c09eda678d6
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-09-03 06:46:05 +0330

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -188,7 +188,8 @@ server {
 		proxy_set_header Host \$host;
 		proxy_set_header X-Real-IP \$remote_addr;
 		proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-  		proxy_set_header CF-IPCountry \$http_cf_ipcountry;
+  		#proxy_set_header CF-IPCountry \$http_cf_ipcountry;
+    		#proxy_set_header CF-IP \$realip_remote_addr;
 		if (\$content_type ~* "GRPC") {
 			grpc_pass grpc://127.0.0.1:\$fwdport\$is_args\$args;
 			break;
