838a83c4279e1b01396217b6fe2aa939dc9c6a2a
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-10-21 21:39:59 +0330

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -35,8 +35,9 @@ done
 UNINSTALL_XUI(){
 	printf 'y\n' | x-ui uninstall
 	rm -rf "/etc/x-ui/" "/usr/local/x-ui/" "/usr/bin/x-ui/"
-	$Pak -y remove nginx nginx-common nginx-core nginx-full python3-certbot-nginx tor
-	$Pak -y purge nginx nginx-common nginx-core nginx-full python3-certbot-nginx tor
+	for i in nginx nginx-common nginx-core nginx-full python3-certbot-nginx tor; do
+		$Pak -y remove $i
+	done
 	$Pak -y autoremove
 	$Pak -y autoclean
 	$Pak -y clean all
