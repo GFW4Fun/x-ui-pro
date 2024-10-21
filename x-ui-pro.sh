cd810a5243541fad973f4da76061706932991d9d
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-10-21 17:30:14 +0330

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -66,7 +66,7 @@ fi
 if [[ ${INSTALL} == *"y"* ]]; then
 	$Pak -y update
 	$Pak -y install epel-release
-	$Pak -y install curl nginx certbot python3-certbot-nginx sqlite3 sqlite tor
+	$Pak -y install curl nginx certbot python3-certbot-nginx sqlite3 tor sqlite
 	systemctl daemon-reload
  	systemctl enable nginx.service
   	systemctl enable tor.service
