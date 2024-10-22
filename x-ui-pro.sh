d9c10f89a73414150f733f774399fe7ee600872e
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-10-22 13:19:49 +0330

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -69,7 +69,7 @@ if [[ ${INSTALL} == *"y"* ]]; then
 	systemctl daemon-reload
  	systemctl enable nginx.service
   	systemctl enable tor.service
-	systemctl start tor
+	systemctl start nginx tor
 fi
 systemctl stop nginx 
 fuser -k 80/tcp 80/udp 443/tcp 443/udp 2>/dev/null
