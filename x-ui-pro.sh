cf7f01d5fa5788e37feba9c9a9e2b951c32cf62c
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-10-22 13:24:36 +0330

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -105,14 +105,15 @@ pid /run/nginx.pid;
 include /etc/nginx/modules-enabled/*.conf;
 events {worker_connections 2048;}
 http {
-	sendfile            on;
-	tcp_nopush          on;
-	tcp_nodelay         on;
-	keepalive_timeout   65;
+	gzip on;
+	sendfile on;
+	tcp_nopush on;
+	tcp_nodelay on;
+	keepalive_timeout 65;
 	types_hash_max_size 4096;
 	access_log /var/log/nginx/access.log;
 	error_log /var/log/nginx/error.log;
- 	default_type application/octet-stream;
+	default_type application/octet-stream;
 	include /etc/nginx/*.types;
 	include /etc/nginx/conf.d/*.conf;
 	include /etc/nginx/sites-enabled/*;
