260f8a80fb03c74b9da20e48d78a5bf6819d8159
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-10-28 23:34:47 +0330

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -156,11 +156,12 @@ fi
 }
 ###################################Install X-UI#########################################################
 if ! systemctl is-active --quiet x-ui; then
-	PANEL=( "https://raw.githubusercontent.com/alireza0/x-ui/master/install.sh"
-		"https://raw.githubusercontent.com/mhsanaei/3x-ui/master/install.sh"
+	xuiVer=${xuiVer:-master}
+	PANEL=( "https://raw.githubusercontent.com/alireza0/${xuiVer}/install.sh"
+		"https://raw.githubusercontent.com/mhsanaei/3x-ui/${xuiVer}/install.sh"
 	)
 
-	printf 'n\n' | bash <(wget -qO- "${PANEL[$PNLNUM]}")
+	printf 'n\n' | bash <(wget -qO- "${PANEL[$PNLNUM]}") ${xuiVer}
  
 	if ! systemctl is-enabled --quiet x-ui; then
 		systemctl daemon-reload
