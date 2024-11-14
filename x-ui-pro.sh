60aef6eebf9d01c31aed28f8fb531c0f71e92c30
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-11-14 06:30:15 -0800

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -465,7 +465,7 @@ if systemctl is-active --quiet x-ui || [ -e /etc/systemd/system/x-ui.service ]; 
 	hrline
 	IPInfo=$(curl -Ls "https://ipapi.co/json" || curl -Ls "https://ipinfo.io/json")
 	msg "Server: ${IP4} | $(uname -n) | $(echo "${IPInfo}" | jq -r '.org, .country' | paste -sd' | ')"
-	printf "\033[1;37;40m CPU: %s/%s Core | RAM: %s | HDD: %s | OS: %s\033[0m\n" \
+	printf "\033[1;34;40m CPU: %s/%s Core | RAM: %s | HDD: %s | OS: %s\033[0m\n" \
 	"$(uname -i)"  "$(nproc)" "$(free -h | awk '/^Mem:/{print $2}')" \
 	"$(lsblk -d -o SIZE --noheadings | tr '\n' ', ' | sed 's/, $//')" \
 	"$(hostnamectl | awk -F: '/Operating System/{print $2}' | xargs)"
