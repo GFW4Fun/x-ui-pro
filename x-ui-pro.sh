a8a56cf576199f8cfaec084ab6b54d43bbcd2afc
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-11-18 08:07:00 -0800

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#################### x-ui-pro v11.4.1 @ github.com/GFW4Fun ##############################################
+#################### x-ui-pro v11.4.2 @ github.com/GFW4Fun ##############################################
 [[ $EUID -ne 0 ]] && { echo "not root!"; exec sudo "$0" "$@"; }
 msg()     { echo -e "\e[1;37;40m $1 \e[0m";}
 msg_ok()  { echo -e "\e[1;32;40m $1 \e[0m";}
@@ -265,7 +265,8 @@ fi
 ###################################Install X-UI#########################################################
 if ! systemctl is-active --quiet x-ui; then
 	[[ "$PNLNUM" =~ ^[0-2]+$ ]] || PNLNUM=1	
-	if [[ -z "$VERSION" || "$VERSION" = "last" ]]; then VERSION="master"
+ 	VERSION=$(echo "$VERSION" | tr -d '[:space:]')
+	if [[ -z "$VERSION" || "$VERSION" != *.* || "$VERSION" = "last" ]]; then VERSION="master"
 	else [[ $PNLNUM = "1" ]] && VERSION="v${VERSION#v}" || VERSION="${VERSION#v}" ; fi	
 	PANEL=( "https://raw.githubusercontent.com/alireza0/x-ui/${VERSION}/install.sh"
 		"https://raw.githubusercontent.com/mhsanaei/3x-ui/${VERSION}/install.sh"
