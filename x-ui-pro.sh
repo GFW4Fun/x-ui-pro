8cc2dac64113e91b7ed82d1e22a58b327e2e0027
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-12-03 04:08:57 -0800

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -137,7 +137,8 @@ if [[ "${UNINSTALL}" == *"y"* ]]; then
 	echo "python3-certbot-nginx nginx tor v2ray v2raya" | xargs -n 1 $Pak -y remove
 
 	for service in nginx tor x-ui warp-plus v2ray v2raya; do
-		systemctl stop "$service" && systemctl disable "$service"
+		systemctl stop "$service" 
+		systemctl disable "$service"
 	done
 
 	rm -rf /etc/warp-plus/ /etc/v2raya/ /etc/nginx/sites-enabled/
