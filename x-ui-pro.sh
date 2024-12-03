72c02f6fa33f31e8d0ddafcf3ecb961bde0206b3
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-12-03 04:31:39 -0800

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -144,7 +144,7 @@ if [[ "${UNINSTALL}" == *"y"* ]]; then
 	rm -rf /etc/warp-plus/ /etc/v2raya/ /etc/nginx/sites-enabled/
 	crontab -l | grep -v "nginx\|systemctl\|x-ui\|v2ray" | crontab -
 	
-	type x-ui &> /dev/null && printf 'y\n' | x-ui uninstall
+	command -v x-ui &> /dev/null && printf 'y\n' | x-ui uninstall
 	
 	clear && msg_ok "Completely Uninstalled!" && exit 1
 fi
