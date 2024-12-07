95e7f2b8f3b7b50740333bc677a5cafe4cdc37f5
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2024-12-07 10:33:25 -0800

    Update x-ui-pro.sh

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#################### x-ui-pro v11.7.1 @ github.com/GFW4Fun ##############################################
+#################### x-ui-pro v11.7.2 @ github.com/GFW4Fun ##############################################
 [[ $EUID -ne 0 ]] && { echo "not root!"; exec sudo "$0" "$@"; }
 msg()     { echo -e "\e[1;37;40m $1 \e[0m";}
 msg_ok()  { echo -e "\e[1;32;40m $1 \e[0m";}
@@ -442,7 +442,7 @@ EOF
 service_enable "warp-plus"
 ##########################################Install v2ray-core + v2rayA-webui#############################
 sudo sh -c "$(wget -qO- https://github.com/v2rayA/v2rayA-installer/raw/main/installer.sh)" @ --with-xray
-service_enable "v2ray" "v2raya" "sing-box" "x-ui"
+service_enable "v2raya" "sing-box" "x-ui"
 ######################cronjob for ssl/reload service/cloudflareips######################################
 crontab -l | grep -v "nginx\|systemctl\|x-ui\|v2ray" | crontab -
 (crontab -l 2>/dev/null; echo "0 0 * * * sudo su -c 'x-ui restart > /dev/null 2>&1 && systemctl reload v2ray v2raya warp-plus tor';") | crontab -
