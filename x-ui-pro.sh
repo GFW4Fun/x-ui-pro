b2506ea094d4cba02205774f983bdb95bfb26da4
Xue Xianliang 自由的世界 <146182012+GFW4Fun@users.noreply.github.com>
2025-05-24 13:17:35 +0330

    Update x-ui-pro.sh
    - Fixed Sqlite

--- x-ui-pro.sh
+++ x-ui-pro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#################### x-ui-pro v11.8.7 @ github.com/GFW4Fun ##############################################
+#################### x-ui-pro v11.8.8 @ github.com/GFW4Fun ##############################################
 [[ $EUID -ne 0 ]] && { echo "not root!"; exec sudo "$0" "$@"; }
 msg()     { echo -e "\e[1;37;40m $1 \e[0m";}
 msg_ok()  { echo -e "\e[1;32;40m $1 \e[0m";}
@@ -167,7 +167,6 @@ $Pak -y update
 for pkg in epel-release cronie psmisc unzip curl nginx nginx-full certbot python3-certbot-nginx sqlite sqlite3 jq openssl tor tor-geoipdb; do
   dpkg -l "$pkg" &> /dev/null || rpm -q "$pkg" &> /dev/null || $Pak -y install "$pkg"
 done
-sudo ln -s /usr/bin/sqlite3 /usr/local/bin/sqlite3
 service_enable "nginx" "tor" "cron" "crond"
 ############################### Get nginx Ver and Stop ##################################################
 vercompare() { 
